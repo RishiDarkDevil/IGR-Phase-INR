277c6a3dd4f28f6617f164c0814672e2fa13493a
RishiDarkDevil <rishi8001100192@gmail.com>
2023-04-19 10:44:17 +0530

    Minor Fix

--- code/reconstruction/run.py
+++ code/reconstruction/run.py
@@ -358,8 +358,8 @@ class ReconstructionRunner:
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--points_batch', type=int, default=10000, help='point batch size')
-    parser.add_argument('--nepoch', type=int, default=10, help='number of epochs to train for')
+    parser.add_argument('--points_batch', type=int, default=1000, help='point batch size')
+    parser.add_argument('--nepoch', type=int, default=100000, help='number of epochs to train for')
     parser.add_argument('--conf', type=str, default='setup.conf')
     parser.add_argument('--expname', type=str, default='single_shape')
     parser.add_argument('--gpu', type=str, default='auto', help='GPU to use [default: GPU auto]')
